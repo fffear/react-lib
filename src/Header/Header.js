4c20d7a0ac3b8c06c55a458512e75e4e3a5f119c
Kevin Vuong <c_k.k@hotmail.com>
2020-02-19 16:17:01 +1100

    add header

---
+++ src/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import Modal from "./Modal/Modal";
+import classes from "./Header.module.css";
+
+const header = props => {
+  return (
+    <header className={classes.Header}>
+      <h1>My Library</h1>
+      {/* <Modal buttonText="Add New Book" addNewBook={props.addNewBook} /> */}
+    </header>
+  );
+};
+
+export default header;
