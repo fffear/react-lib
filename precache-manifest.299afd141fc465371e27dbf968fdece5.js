72c8a53dfd644e469d93de2f6342c4d2fe92aa68
Kevin Vuong <c_k.k@hotmail.com>
2020-02-19 21:13:53 +1100

    Updates

---
+++ precache-manifest.299afd141fc465371e27dbf968fdece5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e57361410ce4639f60c86e35b371cf9c",
+    "url": "/react-lib/index.html"
+  },
+  {
+    "revision": "ef4767b6d010c551d049",
+    "url": "/react-lib/static/css/main.f0b0b42f.chunk.css"
+  },
+  {
+    "revision": "6390ba810eb88b999bdc",
+    "url": "/react-lib/static/js/2.cd6920a2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-lib/static/js/2.cd6920a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef4767b6d010c551d049",
+    "url": "/react-lib/static/js/main.aa0e4391.chunk.js"
+  },
+  {
+    "revision": "7e28b26d561ab40d8c97",
+    "url": "/react-lib/static/js/runtime-main.0deaf1c3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-lib/precache-manifest.ac28884431cdfbe3be0721105940ca22.js"
+  "/react-lib/precache-manifest.299afd141fc465371e27dbf968fdece5.js"
 );
 
 self.addEventListener('message', (event) => {
